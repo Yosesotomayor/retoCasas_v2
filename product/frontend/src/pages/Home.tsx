5fc62d179d972c207277c28b3ca4eb66d0aa9842
A01750817 <ivan_ramost@outlook.com>
2025-08-26 13:03:59 -0600

    feat: integrate Tailwind CSS and refactor App component
    - Added Tailwind CSS and related dependencies to package.json
    - Refactored App component to use Home component
    - Created Home component with a welcome message and Tailwind styling
    - Updated index.css to include Tailwind directives
    - Configured Vite to use Tailwind CSS plugin

--- product/frontend/src/pages/Home.tsx
+++ product/frontend/src/pages/Home.tsx
@@ -4,6 +4,9 @@ export default function Home() {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
+                <div className="p-6 rounded-xl bg-yellow-300 text-black font-extrabold">
+                 Tailwind v4 OK
+                </div>
         </div>
     );
 }
